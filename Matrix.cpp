6111fce6fcfc4310d3208731c1741050c34795fa
OatLatte11 <109826753+HazelZhaoo@users.noreply.github.com>
2023-03-18 22:38:00 -0700

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,5 +1,6 @@
 #include "Matrix.hpp"
 #include <iterator>
+
 Matrix::MyIterator::MyIterator(Node* some_node , Node* some_head):current_node(some_node) , startOfMatrix(some_head){}
 
 Matrix::MyIterator& Matrix::MyIterator::operator++() //prefix
@@ -16,7 +17,7 @@ Matrix::MyIterator& Matrix::MyIterator::operator++() //prefix
     return *this;
 }
 
-Matrix::MyIterator& Matrix::MyIterator::operator++(int dummy) //postfix
+Matrix::MyIterator Matrix::MyIterator::operator++(int dummy) //postfix
 {
     MyIterator temp(*this);
     ++(*this);
@@ -86,17 +87,17 @@ Matrix::Matrix(int** array , int row_size , int col_size)
     
 Matrix::~Matrix()
 {
-    Node* temp = head;
+    Node* temp = head; //set a temporary pointer
     while(temp != nullptr)
     {
-        Node* delete_row = head;
-        while(delete_row != nullptr)
+        Node* delete_row = head; //assign the first row as the now to be delete
+        while(delete_row != nullptr) //delete nodes horizontally
         {
-            Node* delete_node = delete_row;
+            Node* delete_node = delete_row; // hold the first node
             delete_row = delete_row->next_col;
-            delete delete_node;
+            delete delete_node; //delete that node
         }
-        temp = temp->next_row;
+        temp = temp->next_row; // go to the next row and do the same thing
         
     }
 }
@@ -136,9 +137,63 @@ Matrix:: Matrix(const Matrix& copy_m)
         ++count;
     }
 }
- 
- 
+
+Matrix& Matrix::operator=(const Matrix& obj)
+{
+     if (this == &obj) // self-assignment check
+     {
+         return *this;
+     }
+     
+     // Delete the current matrix
+    Node* traverse_ptr = head; //tried using iterator but cant compare to nullptr
+    while(traverse_ptr != nullptr)
+    {
+        Node* delete_row = head; //assign the first row as the now to be delete
+        while(delete_row != nullptr) //delete nodes horizontally
+        {
+            Node* delete_node = delete_row; // hold the first node
+            delete_row = delete_row->next_col;
+            delete delete_node; //delete that node
+        }
+        traverse_ptr = traverse_ptr ->next_row; // go to the next row and do the same thing
+        
+    }
+     
+     // Copy the other matrix
+     head = new Node();
+     Node* current_col = head;
+     num_rows = obj.num_rows;
+     num_cols = obj.num_cols;
+     for (int i = 0; i < num_cols; i++)
+     {
+         current_col->next_col = new Node();
+         current_col = current_col->next_col;
+     }
+     
+     Node* temp = head;
+     for (int i = 0; i < num_cols; i++)
+     {
+         Node* new_head = head;
+         for (int j = 0; j < num_rows; j++)
+         {
+             new_head->next_row = new Node();
+             new_head = new_head->next_row;
+         }
+         temp = temp->next_col;
+     }
+     
+     MyIterator copy_it(obj.head, obj.head->next_row);
+     MyIterator calling_obj(head, nullptr);
+     int count = 0;
+     while (count < num_rows*num_cols)
+     {
+         *calling_obj = *copy_it;
+         ++calling_obj;
+         ++copy_it;
+         ++count;
+     }
+     
+     return *this;
+}
     
-    
- 
- 
