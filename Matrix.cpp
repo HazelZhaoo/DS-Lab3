16bad3cbde9501e8a1b51e397af8773613ab037c
OatLatte11 <109826753+HazelZhaoo@users.noreply.github.com>
2023-03-18 22:57:41 -0700

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -197,3 +197,32 @@ Matrix& Matrix::operator=(const Matrix& obj)
      return *this;
 }
     
+Node* Matrix::getRow(int row_num) //assuming the index start at 0//
+{
+    if(row_num >= num_rows)
+        throw runtime_error("Row number cannot exceed the number of rows!");
+
+    Node* temp = head;//does the row and column start at 0 or 1
+    for(int i = 0 ; i < row_num ; i++)
+    {
+        temp = temp->next_row;
+    }
+    
+    return temp;
+}
+
+Node* Matrix::getCol(int col_num)
+{
+    if(col_num >= num_cols)
+    {
+        throw runtime_error("Column number cannot exceed the number of columns!");
+    }
+    
+    Node* temp = head;
+    for(int i = 0 ; i < col_num ; i++)
+    {
+        temp = temp->next_col;
+    }
+    
+    return temp;
+}
