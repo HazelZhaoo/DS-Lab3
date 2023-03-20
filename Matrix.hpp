b9ae2c9c43889f2c6cbfd639f4527e6d9c31bf19
OatLatte11 <109826753+HazelZhaoo@users.noreply.github.com>
2023-03-19 23:21:30 -0700

    Updated with files from Aniyah

--- Matrix.hpp
+++ Matrix.hpp
@@ -10,57 +10,61 @@ using namespace std;
 #ifndef Matrix_hpp
 #define Matrix_hpp
 
- 
+template <typename T>
 class Node
 {
 public:
     int value;
-    Node* next_col;
-    Node* next_row;
-    
-    Node():value(0) , next_col(nullptr) , next_row(nullptr){};
-    Node(Node* ptr , int val , Node* next_rowptr):next_row(next_rowptr) , value(val) , next_col(ptr) {};
+    Node<T>* next_col;
+    Node<T>* next_row;
+
+    Node() :value(0), next_col(nullptr), next_row(nullptr) {};
+    Node(Node<T>* ptr, int val, Node<T>* next_rowptr) :next_row(next_rowptr), value(val), next_col(ptr) {};
 };
 
+template <typename T>
 class Matrix
 {
 private:
-    Node* head; //a pointer to the head of the matrix//
+    Node<T>* head; //a pointer to the head of the matrix//
     int num_rows;
     int num_cols;
 public:
-    Matrix();
-    Matrix(int** array, int row_size , int col_size);  
-    Matrix(const Matrix& copy_m);
-    Matrix& operator =(const Matrix& obj);
-    Matrix(const Matrix&& obj);
-    Matrix& operator=(const Matrix&& obj);
-    ~Matrix();
-    Node* getRow(int row_num); //throw exception
-    Node* getCol(int col_num);//throw exception
-    Matrix transpose(int Matrix); // the user shouldnt be aware of the node type // should the 2d array be passed instead//
-    Matrix operator+(Matrix obj); //throw exception if row not equal row
-    Matrix operator*(Matrix obj); //throw exception if not multiplicable
-   friend ifstream& operator>>(ifstream& input, Matrix& obj);
-   friend ostream& operator<<(ostream& output , Matrix& obj);
+    Matrix();//--
+    Matrix(int** array, int row_size, int col_size);//--
+    Matrix(const Matrix<T>& copy_m);//--
+    Matrix<T>& operator =(const Matrix<T>& obj);
+    Matrix(const Matrix<T>&& obj);
+    Matrix<T>& operator=(const Matrix<T>&& obj);
+    ~Matrix();//--
+    Node<T>* getRow(int row_num); //throw exception
+    Node<T>* getCol(int col_num);//throw exception
 
-     
-    class MyIterator : public std::iterator<std::forward_iterator_tag  , int>
+    //getElement
+
+    Matrix<T> transpose(int Matrix); // the user shouldnt be aware of the node type // should the 2d array be passed instead//
+    Matrix<T> operator+(Matrix<T> obj); //throw exception if row not equal row
+    Matrix<T> operator*(Matrix<T> obj); //throw exception if not multiplicable
+   
+    friend ostream& operator<<(ostream& output, Matrix<T>& obj);
+
+
+    class MyIterator : public std::iterator<std::forward_iterator_tag, int>
     {
     private:
-        Node* current_node;
-        Node* startOfMatrix;
+        Node<T>* current_node;
+        Node<T>* startOfMatrix;
     public:
-        MyIterator(Node* some_node , Node* some_head);
+        MyIterator(Node<T>* some_node, Node<T>* some_head);
         MyIterator& operator++();
         MyIterator& operator++(int dummy); //pre post fix
         int& operator*(); //dereference
         int* operator->();
         bool operator ==(const MyIterator& it);
         bool operator !=(const MyIterator& it);
-       
+
     };
-    
+
 };
 
 #endif /* Matrix_hpp */
