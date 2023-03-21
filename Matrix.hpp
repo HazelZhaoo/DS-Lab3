1b5aee7c71f484e5293e5c2cd73b2034ed36510a
OatLatte11 <109826753+HazelZhaoo@users.noreply.github.com>
2023-03-20 20:40:01 -0700

    Update Matrix.hpp
    added clear funciton

--- Matrix.hpp
+++ Matrix.hpp
@@ -9,6 +9,8 @@
 using namespace std;
 #ifndef Matrix_hpp
 #define Matrix_hpp
+#define MAX_COL_SIZE 10
+
 
  
 class Node
@@ -19,6 +21,7 @@ public:
     Node* next_row;
     
     Node():value(0) , next_col(nullptr) , next_row(nullptr){};
+    Node(int val) : value(val), next_row(nullptr), next_col(nullptr) {};
     Node(Node* ptr , int val , Node* next_rowptr):next_row(next_rowptr) , value(val) , next_col(ptr) {};
 };
 
@@ -30,19 +33,19 @@ private:
     int num_cols;
 public:
     Matrix();
-    Matrix(int** array, int row_size , int col_size);  
+    Matrix(int array[][MAX_COL_SIZE], int row_size, int col_size);  
     Matrix(const Matrix& copy_m);
     Matrix& operator =(const Matrix& obj);
     Matrix(const Matrix&& obj); 
     Matrix& operator=(const Matrix&& obj);
     ~Matrix();
-    Node* getRow(int row_num); //throw exception
-    Node* getCol(int col_num);//throw exception
-    Matrix transpose(int Matrix); // the user shouldnt be aware of the node type // should the 2d array be passed instead//
+    void clear();
+    Node* getRow(int row_num) const; //throw exception
+    Node* getCol(int col_num) const;//throw exception
+    Matrix& transpose(); // the user shouldnt be aware of the node type // should the 2d array be passed?// instead//
     Matrix operator+(Matrix obj); //throw exception if row not equal row
     Matrix operator*(Matrix obj); //throw exception if not multiplicable
-   friend ifstream& operator>>(ifstream& input, Matrix& obj);
-   friend ostream& operator<<(ostream& output , Matrix& obj);
+    friend ostream& operator<<(ostream& output , Matrix& obj);
 
      
     class MyIterator : public std::iterator<std::forward_iterator_tag  , int>
@@ -58,7 +61,9 @@ public:
         int* operator->();
         bool operator ==(const MyIterator& it);
         bool operator !=(const MyIterator& it);
-       
+        MyIterator& operator=(const MyIterator& it);
+        
+       //should consider implementing the allocate function to reuse//
     };
     
 };
