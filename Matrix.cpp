0f8b8bed7a34491dc495b539dc19d5e8ea00b4d1
OatLatte11 <109826753+HazelZhaoo@users.noreply.github.com>
2023-03-21 20:46:32 -0700

    updated with a copy function
    custom copy function added to be able to called by copy constructor and assignment operator

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,27 +1,31 @@
 #include "Matrix.hpp"
 #include <iterator>
 
-Matrix::MyIterator::MyIterator(Node* some_node , Node* some_head):current_node(some_node) , startOfMatrix(some_head){}
+Matrix::MyIterator::MyIterator(Node* some_node)
+{
+    current_node = some_node;
+    row_start = some_node;
+}
 
 Matrix::MyIterator& Matrix::MyIterator::operator++() //prefix
 {
-    if (current_node != nullptr) {
+    if (current_node->next_row != nullptr) {
         // If there is a next element in the current row, move to it
-        current_node = current_node->next_col;
+        current_node = current_node->next_row;
     }
     else {
-        // If we reached the end of the current row, move to the next row
-        current_node = startOfMatrix->next_row; // Set current node to beginning of next row
-        startOfMatrix = current_node; // Update start of matrix to current node
+        // If there is no next element in the current row, move to the first element of the next row
+        row_start = row_start->next_col;
+        current_node = row_start;
     }
     return *this;
 }
 
-
 Matrix::MyIterator Matrix::MyIterator::operator++(int dummy) //postfix
 {
     MyIterator temp(*this);
-    ++(*this);
+    current_node = current_node->next_row;
+    current_node = current_node->next_col;
      
     return temp;
 }
@@ -33,7 +37,7 @@ int& Matrix::MyIterator::operator*()
 
 int* Matrix::MyIterator::operator->()
 {
-    return &current_node -> value; //& is the address of the pointer
+    return &(current_node -> value); //& is the address of the pointer
 }
 
 bool Matrix::MyIterator::operator==(const MyIterator& it)
@@ -43,11 +47,11 @@ bool Matrix::MyIterator::operator==(const MyIterator& it)
 
 bool Matrix::MyIterator::operator!=(const MyIterator& it)
 {
-    if (current_node == nullptr || it.current_node == nullptr) {
-            return current_node != it.current_node; // handles case where one node is nullptr
-        }
+//    if (current_node == nullptr || it.current_node == nullptr) {
+//            return current_node != it.current_node; // handles case where one node is nullptr
+//        }
     return current_node != it.current_node;
-    }
+}
  
 
 Matrix::MyIterator& Matrix::MyIterator::operator=(const MyIterator& it)
@@ -55,12 +59,11 @@ Matrix::MyIterator& Matrix::MyIterator::operator=(const MyIterator& it)
     if(this != &it)
     {
         current_node = it.current_node;
-        startOfMatrix = it.startOfMatrix;
+        row_start = it.row_start;
     }
     
     return *this;
 }
-
 
 Matrix::Matrix():head(nullptr) , num_rows(0) , num_cols(0){};
 
@@ -70,6 +73,12 @@ Matrix::Matrix(int array[][MAX_COL_SIZE], int row_size, int col_size)
     Node* current_col = head;
     num_rows = row_size;
     num_cols = col_size;
+
+    // Resize the array if necessary
+        if (num_cols > MAX_COL_SIZE)
+        {
+            num_cols = MAX_COL_SIZE;
+        }
 
     // Create nodes horizontally based on the number of columns
     for(int i = 0; i < num_cols; i++)
@@ -100,24 +109,7 @@ Matrix::~Matrix() //destructor
 // Copy constructor
 Matrix::Matrix(const Matrix& copy_m)
 {
-    // Initialize new matrix with same dimensions
-    num_rows = copy_m.num_rows;
-    num_cols = copy_m.num_cols;
-    head = new Node();
-
-    // Copy each element from copy_m to the new matrix
-    for (int i = 0; i < num_rows; i++) {
-        Node* current_row = getRow(i);
-        Node* copy_row = copy_m.getRow(i);
-
-        MyIterator it(current_row, head);
-        MyIterator copy_it(copy_row, copy_m.head); //create two iterators to copy them with a loop
-        for (int j = 0; j < num_cols; j++) {
-            *it = *copy_it;
-            it++;
-            copy_it++;
-        }
-    }
+    copyFrom(copy_m);
 }
 
 
@@ -129,34 +121,7 @@ Matrix& Matrix::operator=(const Matrix& obj) //assignment operator
      }
      
     clear();
-     
-     // Copy the other matrix
-     head = new Node(obj.head->value); //head is copied
-     Node* current_col = head;
-     Node* obj_head = obj.head->next_col; //will start copying from the next element
-     num_rows = obj.num_rows;
-     num_cols = obj.num_cols;
-     for (int i = 1; i < num_cols; i++)
-     {
-         current_col->next_col = new Node(obj_head->value); //create node and copy the first row as well//
-         current_col = current_col->next_col;
-         obj_head = obj_head->next_col;
-     }
-     
-     Node* obj_temp = obj.head->next_row; // a pointer to the second row of obj
-     Node* temp = head->next_row; // a pointer to the second row
-     for (int i = 1; i < num_cols; i++)
-     {
-         Node* new_head = temp;
-         for (int j = 0; j < num_rows; j++)
-         {
-             new_head->next_row = new Node(obj_temp->value);
-             new_head = new_head->next_row;
-             obj_temp = obj_temp->next_row;
-         }
-         temp = temp->next_col;
-     }
-     
+    copyFrom(obj);
      return *this;
 }
 
@@ -179,34 +144,44 @@ void Matrix::clear()
     
 }
 
-Node* Matrix::getRow(int row_num) const //assuming the index start at 0//
+int* Matrix::getRow(int row_num) const //assuming the index start at 0//
 {
     if(row_num >= num_rows)
         throw runtime_error("Row number cannot exceed the number of rows!");
-
+    
     Node* temp = head;//does the row and column start at 0 or 1
-    for(int i = 0 ; i < row_num ; i++)
+    int* arr = new int[num_cols];
+    
+    for(int i = 0 ; i < num_rows ; i++)
     {
         temp = temp->next_row;
     }
     
-    return temp;
+    for (int i = 0; i < num_cols && temp != nullptr; i++, temp = temp->next_col)
+    {
+        arr[i] = temp->value;
+    }
+ 
+    return arr;
 }
 
-Node* Matrix::getCol(int col_num) const
+int* Matrix::getCol(int col_num) const
 {
     if(col_num >= num_cols)
     {
         throw runtime_error("Column number cannot exceed the number of columns!");
     }
-    
+    int* arr = new int[num_rows];
     Node* temp = head;
     for(int i = 0 ; i < col_num ; i++)
     {
         temp = temp->next_col;
     }
-    
-    return temp;
+    for(int i = 0 ; i < num_rows && temp != nullptr ; i++ , temp = temp->next_row)
+    {
+        arr[i] = temp->value;
+    }
+    return arr;
 }
 
 Matrix& Matrix::transpose()
@@ -218,8 +193,8 @@ Matrix& Matrix::transpose()
     
     for(int i = 0 ; i < this->num_rows ; i++)
     {
-        Node* current_row = this->getRow(i); //get the row
-        Node* current_node = current_row; //now hold the node to traverse through it
+        int* current_row = this->getRow(i); //get the row
+        int* current_node = current_row; //now hold the node to traverse through it
         for(int j = 0 ; j < this->num_cols ; j++ )
         {
             ptr_to_head->value = current_node->value; //assign value to corresponding node
@@ -230,19 +205,33 @@ Matrix& Matrix::transpose()
     }
     return trans;
 }
- 
-ostream& operator<<(ostream& output, Matrix& obj)
-{
-    Matrix::MyIterator it(obj.head, obj.head->next_row);
-    
-    for(int i = 0; i < obj.num_rows; i++)
-    {
-        for(int j = 0; j < obj.num_cols; j++, it++)
-        {
-            output << *it << " ";
-        }
-        output << "\n"; 
-    }
-    return output;
-}
 
+void Matrix:: copyFrom(const Matrix& obj)
+{
+    // Copy the other matrix
+    head = new Node(obj.head->value); //head is copied
+    Node* current_col = head;
+    Node* obj_head = obj.head->next_col; //will start copying from the next element
+    num_rows = obj.num_rows;
+    num_cols = obj.num_cols;
+    for (int i = 1; i < num_cols; i++)
+    {
+        current_col->next_col = new Node(obj_head->value); //create node and copy the first row as well//
+        current_col = current_col->next_col;
+        obj_head = obj_head->next_col;
+    }
+    
+    Node* obj_temp = obj.head->next_row; // a pointer to the second row of obj
+    Node* temp = head->next_row; // a pointer to the second row
+    for (int i = 1; i < num_cols; i++)
+    {
+        Node* new_head = temp->next_row;
+        for (int j = 0; j < num_rows; j++)
+        {
+            new_head->next_row = new Node(obj_temp->value);
+            new_head = new_head->next_row;
+            obj_temp = obj_temp->next_row;
+        }
+        temp = temp->next_col;
+    }
+}
