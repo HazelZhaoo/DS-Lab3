a155a4680f460248a3fdde81f75859f7464bbfc5
OatLatte11 <109826753+HazelZhaoo@users.noreply.github.com>
2023-03-21 20:47:05 -0700

    Added the copy function

--- Matrix.hpp
+++ Matrix.hpp
@@ -9,10 +9,8 @@
 using namespace std;
 #ifndef Matrix_hpp
 #define Matrix_hpp
-#define MAX_COL_SIZE 10
 
-
- 
+const int MAX_COL_SIZE = 10;
 class Node
 {
 public:
@@ -33,18 +31,19 @@ private:
     int num_cols;
 public:
     Matrix();
-    Matrix(int array[][MAX_COL_SIZE], int row_size, int col_size);  
+    Matrix(int array[][MAX_COL_SIZE], int row_size, int col_size);
     Matrix(const Matrix& copy_m);
     Matrix& operator =(const Matrix& obj);
-    Matrix(const Matrix&& obj); 
-    Matrix& operator=(const Matrix&& obj);
+    // Matrix(const Matrix&& obj);
+    //Matrix& operator=(const Matrix&& obj);
     ~Matrix();
     void clear();
-    Node* getRow(int row_num) const; //throw exception
-    Node* getCol(int col_num) const;//throw exception
+    int* getRow(int row_num) const; //throw exception
+    int* getCol(int col_num) const;//throw exception
     Matrix& transpose(); // the user shouldnt be aware of the node type // should the 2d array be passed?// instead//
     Matrix operator+(Matrix obj); //throw exception if row not equal row
     Matrix operator*(Matrix obj); //throw exception if not multiplicable
+    void copyFrom(const Matrix& obj);
     friend ostream& operator<<(ostream& output , Matrix& obj);
 
      
@@ -52,9 +51,9 @@ public:
     {
     private:
         Node* current_node;
-        Node* startOfMatrix;
+        Node* row_start;
     public:
-        MyIterator(Node* some_node , Node* some_head);
+        MyIterator(Node* some_node); //should change to one paratmeter
         MyIterator& operator++();
         MyIterator operator++(int dummy); //pre post fix
         int& operator*(); //dereference
@@ -62,6 +61,7 @@ public:
         bool operator ==(const MyIterator& it);
         bool operator !=(const MyIterator& it);
         MyIterator& operator=(const MyIterator& it);
+         
         
        //should consider implementing the allocate function to reuse//
     };
